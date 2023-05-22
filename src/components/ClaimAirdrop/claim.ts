e56b0c622ace182c059de3516b84c24aad946d45
Phan Nhat Minh <minh.phan@ekoios.vn>
2023-05-22 21:43:12 +0700

    update

--- src/components/ClaimAirdrop/claim.ts
+++ src/components/ClaimAirdrop/claim.ts
@@ -3,11 +3,11 @@ import ContractCollection from '@/abi/ContractCollection.json';
 
 export const claim = async(tokenId:any, account: any, library: any) => {
     const contract = getContract(
-        "0x297f7F2C68Bf197b748AC65E3C89d05849D2fc8f",
+      "0x297f7F2C68Bf197b748AC65E3C89d05849D2fc8f",
         ContractCollection,
         library,
         account
-      );
+    );
 
-      await contract?.claimAirdrop(tokenId);
+    await contract?.claimAirdrop(tokenId);
 }
